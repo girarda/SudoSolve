f4de5f0f9db0013d0a331b52d08a97f0b3835d00
Alexandre Girard <girarda.92@gmail.com>
2013-03-25 13:14:18 -0400

    Cells preprosessing before OCR.

--- src/com/girarda/sudosolve/sudograb/SudokuGrabber.java
+++ src/com/girarda/sudosolve/sudograb/SudokuGrabber.java
@@ -9,10 +9,12 @@ import org.opencv.core.CvType;
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfPoint;
 import org.opencv.core.MatOfPoint2f;
-import org.opencv.core.Point;
+import org.opencv.core.Rect;
 import org.opencv.core.Scalar;
 import org.opencv.core.Size;
 import org.opencv.imgproc.Imgproc;
+import org.opencv.ml.CvSVM;
+import org.opencv.core.Point;
 
 import android.graphics.Bitmap;
 
@@ -22,14 +24,16 @@ public class SudokuGrabber {
 	private Mat imgMatrix = new Mat();
 	private Mat intermediateMat;
 	private Mat newImg;
-	
+	private CvSVM svm = new CvSVM();
+
+
 	public SudokuGrabber(Bitmap bitmapImg) {
 		originalImg = bitmapImg;
 		bitmapToMatrix(originalImg, imgMatrix);
 		intermediateMat = imgMatrix.clone();
 		convertBGR2Gray(intermediateMat);
 	}
-	
+
 	private void convertBGR2Gray(Mat matrix) {
 		Imgproc.cvtColor(matrix, matrix, Imgproc.COLOR_BGR2GRAY);
 	}
@@ -43,7 +47,8 @@ public class SudokuGrabber {
 		Point[] sudokuGrid =detectSudokuGrid(intermediateMat);
 		Point[] corners = detectCorners(intermediateMat, sudokuGrid);
 		newImg = warpSudokuGrid(corners, imgMatrix);
-		Mat[][] cells = getCells(imgMatrix);
+		Mat[][] cells = getCells(newImg);
+		newImg = cells[3][0];
 		return getConvertedResult();
 	}
 
@@ -95,66 +100,115 @@ public class SudokuGrabber {
 		// Returned corners order in array:
 		// 1 0
 		// 2 3
-		
+
 		MatOfPoint2f approxCurve =  new MatOfPoint2f();
 		MatOfPoint2f source = new MatOfPoint2f(points);
 		Imgproc.approxPolyDP(source, approxCurve, 25.0, true);
-		
+
 		Point[] corners = approxCurve.toArray();
 		for (int i = 0; i < corners.length; i++) {
 			Core.circle(matrix, corners[i], 10, new Scalar(255,0,0));
 		}
 		return corners;
 	}
-	
+
 	private Mat warpSudokuGrid(Point[] corners, Mat matrix) {
 		Point ptTopLeft = corners[1];			Point ptTopRight = corners[0];
 		Point ptBottomLeft= corners[2];			Point ptBottomRight = corners[3];
-		
-		double maxLength = (ptBottomLeft.x-ptBottomRight.x)*(ptBottomLeft.x-ptBottomRight.x) + (ptBottomLeft.y-ptBottomRight.y)*(ptBottomLeft.y-ptBottomRight.y);
-	    double temp = (ptTopRight.x-ptBottomRight.x)*(ptTopRight.x-ptBottomRight.x) + (ptTopRight.y-ptBottomRight.y)*(ptTopRight.y-ptBottomRight.y);
-	    if(temp > maxLength) maxLength = temp;
-	 
-	    temp = (ptTopRight.x-ptTopLeft.x)*(ptTopRight.x-ptTopLeft.x) + (ptTopRight.y-ptTopLeft.y)*(ptTopRight.y-ptTopLeft.y);
-	    if(temp > maxLength) maxLength = temp;
-	 
-	    temp = (ptBottomLeft.x-ptTopLeft.x)*(ptBottomLeft.x-ptTopLeft.x) + (ptBottomLeft.y-ptTopLeft.y)*(ptBottomLeft.y-ptTopLeft.y);
-	    if(temp > maxLength) maxLength = temp;
-	 
-	    maxLength = Math.sqrt((double)maxLength);
-	    
-	    Point[] src = new Point[4];
-	    Point[] dst = new Point[4];
-	    src[0] = ptTopLeft;            dst[0] = new Point(0,0);
-	    src[1] = ptTopRight;        dst[1] = new Point(maxLength-1, 0);
-	    src[2] = ptBottomRight;        dst[2] = new Point(maxLength-1, maxLength-1);
-	    src[3] = ptBottomLeft;        dst[3] = new Point(0, maxLength-1);
-	    
-	    Mat srcMat = new Mat(4,1, CvType.CV_32FC2);
-	    Mat dstMat = new Mat(4,1, CvType.CV_32FC2);
-	    for (int i = 0; i < src.length; i++) {
-	    	srcMat.put(i, 0, new double[]{src[i].x, src[i].y});
-	    	dstMat.put(i, 0, new double[]{dst[i].x, dst[i].y});
-	    }
-	    
-		Mat undistortedGrid = new Mat((int)maxLength, (int)maxLength, CvType.CV_32FC2);
-	    Imgproc.warpPerspective(matrix, undistortedGrid, Imgproc.getPerspectiveTransform(srcMat, dstMat), new Size(maxLength, maxLength));
 
-	    return undistortedGrid;
+		double maxLength = (ptBottomLeft.x-ptBottomRight.x)*(ptBottomLeft.x-ptBottomRight.x) + (ptBottomLeft.y-ptBottomRight.y)*(ptBottomLeft.y-ptBottomRight.y);
+		double temp = (ptTopRight.x-ptBottomRight.x)*(ptTopRight.x-ptBottomRight.x) + (ptTopRight.y-ptBottomRight.y)*(ptTopRight.y-ptBottomRight.y);
+		if(temp > maxLength) maxLength = temp;
+
+		temp = (ptTopRight.x-ptTopLeft.x)*(ptTopRight.x-ptTopLeft.x) + (ptTopRight.y-ptTopLeft.y)*(ptTopRight.y-ptTopLeft.y);
+		if(temp > maxLength) maxLength = temp;
+
+		temp = (ptBottomLeft.x-ptTopLeft.x)*(ptBottomLeft.x-ptTopLeft.x) + (ptBottomLeft.y-ptTopLeft.y)*(ptBottomLeft.y-ptTopLeft.y);
+		if(temp > maxLength) maxLength = temp;
+
+		maxLength = Math.sqrt((double)maxLength);
+
+		Point[] src = new Point[4];
+		Point[] dst = new Point[4];
+		src[0] = ptTopLeft;            dst[0] = new Point(0,0);
+		src[1] = ptTopRight;        dst[1] = new Point(maxLength-1, 0);
+		src[2] = ptBottomRight;        dst[2] = new Point(maxLength-1, maxLength-1);
+		src[3] = ptBottomLeft;        dst[3] = new Point(0, maxLength-1);
+
+		Mat srcMat = new Mat(4,1, CvType.CV_32FC2);
+		Mat dstMat = new Mat(4,1, CvType.CV_32FC2);
+		for (int i = 0; i < src.length; i++) {
+			srcMat.put(i, 0, new double[]{src[i].x, src[i].y});
+			dstMat.put(i, 0, new double[]{dst[i].x, dst[i].y});
+		}
+
+		Mat undistortedGrid = new Mat((int)maxLength, (int)maxLength, CvType.CV_32SC1);
+		Imgproc.warpPerspective(matrix, undistortedGrid, Imgproc.getPerspectiveTransform(srcMat, dstMat), new Size(maxLength, maxLength));
+
+		return undistortedGrid;
 	}
-	
+
 	private Mat[][] getCells(Mat undistortedGrid) {
-	    //draw grid lines
+
+		//draw grid lines
 		Mat[][] cells = new Mat[10][10];
-	    for (int row = 0; row < 9; row++) {
-	    	for (int col = 0; col < 9; col++) {
-		    	Core.circle(undistortedGrid, new Point(undistortedGrid.rows()*row/9, undistortedGrid.rows()*col/9), 5, new Scalar(255,0,0));
-	    		cells[row][col] = undistortedGrid.submat(undistortedGrid.rows()*row/9,undistortedGrid.rows()*(row+1)/9,undistortedGrid.cols()*col/9,undistortedGrid.cols()*(col+1)/9);
-	    	}
-	    }
-	    
-	    //undistortedGrid = undistortedGrid.submat(0, undistortedGrid.rows()/9, 0, undistortedGrid.rows()/9);
-	    return cells;
+		for (int row = 0; row < 9; row++) {
+			for (int col = 0; col < 9; col++) {
+				//Core.circle(undistortedGrid, new Point(undistortedGrid.rows()*row/9, undistortedGrid.rows()*col/9), 5, new Scalar(255,0,0));
+				cells[row][col] = undistortedGrid.submat(undistortedGrid.rows()*row/9,undistortedGrid.rows()*(row+1)/9,undistortedGrid.cols()*col/9,undistortedGrid.cols()*(col+1)/9);
+				cells[row][col] = preprosess(cells[row][col], 10, 10);
+			}
+		}
+		return cells;
+	}
+
+	private Mat preprosess(Mat imgSrc, int newWidth, int newHeight) {
+		Mat result = new Mat(imgSrc.size(), CvType.CV_32F);
+
+		imgSrc.assignTo(result, CvType.CV_32F);
+
+		Mat scaledResult = new Mat();
+
+		convertBGR2Gray(result);
+		Imgproc.GaussianBlur(result, result, new Size(11, 11), 0);
+
+		result.assignTo(result, CvType.CV_8UC1);
+		Imgproc.adaptiveThreshold(result, result, 255, Imgproc.ADAPTIVE_THRESH_MEAN_C, Imgproc.THRESH_BINARY, 5, 2);
+		Core.bitwise_not(result, result);
+
+		List<MatOfPoint> contours = new ArrayList<MatOfPoint>();
+		Imgproc.findContours(result, contours, new Mat(), Imgproc.RETR_LIST, Imgproc.CHAIN_APPROX_SIMPLE);
+		
+		double maxArea = -1;
+		int maxAreaIdx = -1;
+
+		for (int i = 0; i < contours.size(); i++) {
+			MatOfPoint contour = contours.get(i);
+			double contourarea = Imgproc.contourArea(contour);
+			if (contourarea > maxArea) {
+				maxArea = contourarea;
+				maxAreaIdx = i;
+			}
+		}
+		for (int i = 0; i < contours.size(); i++) {
+			if (i == maxAreaIdx) {
+				Imgproc.drawContours(result, contours, maxAreaIdx, new Scalar(255, 255 ,255));
+			}
+			else {
+				Imgproc.drawContours(result, contours, i, new Scalar(0, 0 ,0));
+			}
+		}
+		if (contours.size() > 0) {
+			result = imgSrc.submat(Imgproc.boundingRect(contours.get(maxAreaIdx)));
+		}
+
+		// Dilate img?
+		//dilate(result);
+
+		result.assignTo(result,CvType.CV_8UC1);
+
+		Imgproc.resize(result, scaledResult, new Size(newWidth, newHeight));
+		return scaledResult;
 	}
 
 	private void matrixToBitmap(Mat matrix, Bitmap bitmap) {
